93dbfd18595d654e7d1a1fc38c9792582d5c37af
Michael Scholtz <michael.scholtz@outlook.com>
2015-03-21 11:54:39 +0100

    Renamed header file.

--- src/imagereconstruct.hpp
+++ src/imagereconstruct.hpp
@@ -12,6 +12,6 @@ struct Pixel {
 template<typename T> void ImageReconstruct(cv::Mat& marker, cv::Mat& mask);
 template<typename T> void PropagationStep(cv::Mat& marker, cv::Mat& mask ,int x, int y, int offsetX, int offsetY, std::queue<Pixel>& queue);
 
-#include "template_imagereconstruct.hpp"
+#include "imagereconstruct_t.hpp"
 
 #endif
